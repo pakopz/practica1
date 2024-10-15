d6c5ab06a233f6fdffb94ca22f0a2cce290f79be
pakopz <104804887+pakopz@users.noreply.github.com>
2024-10-14 18:29:58 -0600

    Update hola.py
    suma de dos numeros

--- hola.py
+++ hola.py
@@ -1,1 +1,21 @@
-print ("Hola mundo")
+class Suma:
+    def __init__(self, numero1, numero2):
+        self.numero1 = numero1
+        self.numero2 = numero2
+
+    def calcular_suma(self):
+        return self.numero1 + self.numero2
+
+
+if __name__ == "__main__":
+    try:
+        num1 = int(input("Introduce el primer número: "))
+        num2 = int(input("Introduce el segundo número: "))
+        
+        suma = Suma(num1, num2)
+        
+        resultado = suma.calcular_suma()
+        print(f"La suma de {suma.numero1} y {suma.numero2} es: {resultado}")
+
+    except ValueError:
+        print("Por favor, introduce valores numéricos válidos.")
